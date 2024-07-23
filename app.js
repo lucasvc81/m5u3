7dc2abb519b3f2c5063fc42e39596224da847dac
Lucas VC <lucasvc@hotmail.com>
2024-07-23 16:46:00 -0300

    app.js

--- app.js
+++ app.js
@@ -4,6 +4,8 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+require('dotenv').config();
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -21,6 +23,12 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// Select
+
+pool.query('select * from Empleados').then(function(resultados) {
+  console.log(resultados)
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
